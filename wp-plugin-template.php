349ef51343339e95c166180cd450d305d7e76bbd
Salvador Gascon <66743758+salvadorgascon@users.noreply.github.com>
2020-11-22 19:31:00 +0100

    Create  wp-plugin-template.php

---
+++ wp-plugin-template.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name:     Wordpress plugin template
+ * Plugin URI:      https://github.com/salvadorgascon/wp-plugin-template
+ * Description:     Wordpress plugin template
+ *
+ * Version:         0.0.1
+ *
+ * GitHub URI:      https://github.com/salvadorgascon/wp-plugin-template
+ *
+ * Author:          Salvador Gascon
+ * Author URI:      https://github.com/salvadorgascon
+ */
